bac77824999cfe04910af5e4b146efb515f07a06
varchunsik <akhlyustina82@gmail.com>
2020-10-14 16:46:18 +0500

    Выполнено

---
+++ Математические формулы/Математические формулы/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Математические_формулы
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Введите значение x");
+            var x = Double.Parse(Console.ReadLine());
+
+            var y = F(x);
+
+            Console.WriteLine("f(x)=" + y);
+            Console.ReadKey();
+
+        }
+
+        
+
+
+    }
+}
