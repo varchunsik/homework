c811348baf632b4ca77c3920e5ecac8e670b9359
varchunsik <akhlyustina82@gmail.com>
2020-10-25 22:49:51 +0500

    Выполнено 03

---
+++ 03/03/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace _03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            {
+                Console.WriteLine("Введите значение x: ");
+                var x = double.Parse(Console.ReadLine());
+
+                var y = F(x);
+
+                Console.WriteLine("f(x) = " + y);
+                Console.ReadKey();
+            }
+
+            static double F(double x)
+            {
+                return Math.Sqrt(x + (1 / (Math.Sqrt(Math.Pow(x, 2) + 4))));
+            }
+        }
+    }
+}
