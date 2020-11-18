341aa20ecc61663c0f2cf1750a9d473ee448a935
varchunsik <akhlyustina82@gmail.com>
2020-11-18 23:43:53 +0500

    выполнено

---
+++ 6.1/6.1/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _6._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var a = GetNumber("m");
+            var b = GetNumber("n");
+
+            string answer = "Нет";
+
+            if (EvaluateLogicalExpression(a, b))
+                answer = "Да";
+
+            Console.WriteLine("Верно ли, что хотя бы одно из чисел m или n положительное? - " + answer);
+
+            Console.ReadKey();
+        }
+
+        static bool EvaluateLogicalExpression(int m, int n)
+        {
+
+            return m > 0 || n > 0;
+        }
+
+        static int GetNumber(string numberName)
+        {
+            Console.WriteLine("Введите число " + numberName);
+            return int.Parse(Console.ReadLine());
+        }
+    }
+    
+}
