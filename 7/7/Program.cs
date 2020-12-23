2efcf994002ddd1a5f4397536216ee03977dab92
varchunsik <akhlyustina82@gmail.com>
2020-12-24 02:26:57 +0500

    выполнено 1 вариант

---
+++ 7/7/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _7
+{
+    class Program
+    {
+        static int First(int x)
+        {
+            if (x > 0) return 1;
+            if (x == 0) return 0;
+            return -1;
+        }
+        static void Main()
+        {
+            First(1);
+            Console.WriteLine("Значение функции:" + First(1) );
+            Console.ReadKey();
+        }
+    }
+}
