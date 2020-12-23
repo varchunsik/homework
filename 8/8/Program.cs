d59135f3010e97d3968de1eb447bfe67913d6e4e
varchunsik <akhlyustina82@gmail.com>
2020-12-24 02:56:06 +0500

    выполнено 1вариант

---
+++ 8/8/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Написать программу, которая вычисляет сумму кубов всех целых чисел от a до b включительно. Числа a и b вводятся с клавиатуры (0ab)
+            var a = int.Parse(Console.ReadLine());
+            var b = int.Parse(Console.ReadLine());
+            var res = 0;
+            while(a <= b)
+            {
+                res += a * a * a++;
+            }
+
+            Console.WriteLine("Результат:" + res);
+            Console.ReadKey();
+        }
+    }
+}
